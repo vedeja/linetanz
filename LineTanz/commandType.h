ceda20b49e6ffc7f369e6807070a20749a1e6c92
Stefan Vedeja <stefan@vedeja.se>
2025-02-12 20:44:46 +0100

    feat: add more mixer command types

--- LineTanz/commandType.h
+++ LineTanz/commandType.h
@@ -4,20 +4,32 @@
 enum commandType {
   cmdTypeKeepAlive = 0x01,
   cmdTypeHandshake = 0x03,
+  cmfFirmwareInfo = 0x04,
   cmdTypeMessageSizeControl = 0x06,
   cmdTypeInfo = 0x0E,
-  cmdTypeChannelValues = 0x13
+  cmdTypeChannelValues = 0x13,
+  cmdTypeMeterLayout = 0x15, // aka Broadcast control
+  cmdTypeMeterControl = 0x16,
+  cmdTypeChannelNames = 0x18,
 };
 
 const char* getCommandTypeName(commandType command) {
-    switch (command) {
-        case cmdTypeKeepAlive: return "KeepAlive";
-        case cmdTypeHandshake: return "Handshake";
-        case cmdTypeMessageSizeControl: return "MessageSizeControl";
-        case cmdTypeInfo: return "Info";
-        case cmdTypeChannelValues: return "ChannelValues";
-        default: return "UNKNOWN";
+  switch (command) {
+    case cmdTypeKeepAlive: return "KeepAlive";
+    case cmdTypeHandshake: return "Handshake";
+    case cmfFirmwareInfo: return "FirmwareInfo";
+    case cmdTypeMessageSizeControl: return "MessageSizeControl";
+    case cmdTypeInfo: return "Info";
+    case cmdTypeChannelValues: return "ChannelValues";
+    case cmdTypeMeterLayout: return "MeterLayout";
+    case cmdTypeMeterControl: return "MeterControl";
+    case cmdTypeChannelNames: return "ChannelNames";
+    default: {
+      static char formattedString[50];
+      snprintf(formattedString, 50, "UNKNOWN (0x%02X)", command);
+      return formattedString;
     }
+  }
 }
 
 commandType getCommandType(uint8_t value) {
